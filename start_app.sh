c8698b93cbe665c54a6c0e55c453f507a4942f7b
the julianson <jbazanaguirre@gmail.com>
2023-04-05 20:23:41 -0300

    no username

--- start_app.sh
+++ start_app.sh
@@ -3,6 +3,6 @@
 set -o errexit
 echo "Running migrations, creating super user, and collecting static"
 python manage.py migrate
-python manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL || echo "Superuser already exists"
+python manage.py createsuperuser --noinput --email $DJANGO_SUPERUSER_EMAIL || echo "Superuser already exists"
 echo "Ready for app server"
 gunicorn core.wsgi:application --bind 0.0.0.0:$PORT
