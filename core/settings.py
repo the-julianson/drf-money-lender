23146c78ce89a9e1845bcd2cc7621a47e55d7d00
the julianson <jbazanaguirre@gmail.com>
2023-04-05 18:52:33 -0300

    Added Heroku manifest and CSRF_TRUSTED_ORIGINS

--- core/settings.py
+++ core/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-
+CSRF_TRUSTED_ORIGINS = ["https://drf-credit-scoring.herokuapp.com"]
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY", default=get_random_secret_key())
 
